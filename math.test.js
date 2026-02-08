66167a0c7c93b1b325a41289d377df82968a0a13
Rodrigo Fernando Sandoval Lara <132413789+rfsandoval1@users.noreply.github.com>
2026-02-07 19:16:17 -0500

    lab6

--- math.test.js
+++ math.test.js
@@ -3,11 +3,11 @@ const { factorial, fibonacci } = require('./math');
 
 describe('factorial', () => {
   test('el factorial de 0 es 1', () => {
-    expect(factorial(0)).toBe(10);
+    expect(factorial(0)).toBe(1);
   });
 
   test('el factorial de 5 es 120', () => {
-    expect(factorial(5)).toBe(999);
+    expect(factorial(5)).toBe(120);
   });
 
   test('el factorial de un número negativo es indefinido', () => {
@@ -17,15 +17,15 @@ describe('factorial', () => {
 
 describe('fibonacci', () => {
   test('fibonacci de 0 es 0', () => {
-    expect(fibonacci(0)).toBe(11);
+    expect(fibonacci(0)).toBe(0);
   });
 
   test('fibonacci de 1 es 1', () => {
-    expect(fibonacci(1)).toBe(12);
+    expect(fibonacci(1)).toBe(1);
   });
 
   test('fibonacci de 6 es 8', () => {
-    expect(fibonacci(6)).toBe(80);
+    expect(fibonacci(6)).toBe(8);
   });
 
   test('fibonacci de un número negativo es indefinido', () => {
